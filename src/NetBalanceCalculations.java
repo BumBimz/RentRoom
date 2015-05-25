ba52b1c4c683c2b99c07ed33603fc192c5ba1028
BumBimz <l3uml3imz@gmail.com>
2015-05-25 16:38:44 +0700

    Add Test 'standard_room_one_weekend_should_be_pay_2500'

--- src/NetBalanceCalculations.java
+++ src/NetBalanceCalculations.java
@@ -2,4 +2,8 @@ public class NetBalanceCalculations {
     public int onWeekdayBalance(Room room, int numberOfTheDay) {
         return room.getAmountWeekday() * numberOfTheDay;
     }
+
+    public int onWeekendBalance(Room room, int numberOfTheDay) {
+        return 0;
+    }
 }
--- test/NetBalanceCalculationsTest.java
+++ test/NetBalanceCalculationsTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 public class NetBalanceCalculationsTest {
     NetBalanceCalculations netBalanceCalculations = new NetBalanceCalculations();
-    
+
     @Test
     public void
     standard_room_on_many_weekday(){
@@ -18,5 +18,12 @@ public class NetBalanceCalculationsTest {
     deluxe_room_one_weekday_should_be_pay_4000(){
         DeluxeRoom room = new DeluxeRoom();
         assertEquals(4000, netBalanceCalculations.onWeekdayBalance(room, 1));
+    }
+
+    @Test
+    public void
+    standard_room_one_weekend_should_be_pay_2500() {
+        StandardRoom room = new StandardRoom();
+        assertEquals(2500,netBalanceCalculations.onWeekendBalance(room,1));
     }
 }
